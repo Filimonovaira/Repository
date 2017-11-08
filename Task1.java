b0c53b5ba508756dea1fe352dd0edc218810ade4
Filimonovaira <Filimonovaira96@gmail.com>
2017-11-08 10:03:19 +0300

    Init

---
+++ Task1.java
@@ -0,0 +1,14 @@
+class Task1{
+   public static void main(String [] args){
+      try{
+                 int a= Integer.parseInt(args[0]);
+                 int b= Integer.parseInt(args[1]);
+		 int c=a+b;
+		 System.out.println("Сумма чисел =" + c); 
+	  }
+      catch (Exception e){
+		  System.out.println("Некорректные данные, введите 2 чиса"); 
+	  }
+     }
+}
+      
